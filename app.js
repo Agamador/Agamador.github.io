4d7e4430a0e09e568f907406633ed196c57a2562
Alejandro García <ccmns12@gmail.com>
2022-09-08 02:55:52 +0200

    Cambio de diseño, reestructuración de la web.

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-const links = document.querySelectorAll('.link');
-const sections = document.querySelectorAll('section');
-
-let activeLink = 0;
-
-links.forEach((link, i)=> {
-    link.addEventListener('click', () => {
-    if(activeLink != i){
-        links[activeLink].classList.remove('active');
-        link.classList.add('active');
-        sections[activeLink].classList.remove('active');
-
-        setTimeout(()=>{
-            activeLink = i;
-            sections[i].classList.add('active');
-        },1000)
-    }
-    })
-})
-$.get("nav.html", function(data){
-    $("#nav-placeholder").replaceWith(data);
-});
+function change(str) {
+  document.getElementById("aboutme").style.opacity = "0";
+  document.getElementById("cvitae").style.opacity = "0";
+  document.getElementById("projects").style.opacity = "0";
+  document.getElementById("contact").style.opacity = "0";
+  document.getElementById("aboutmelink").style.color = "#6246ea";
+  document.getElementById("cvitaelink").style.color = "#6246ea";
+  document.getElementById("projectslink").style.color = "#6246ea";
+  document.getElementById("contactlink").style.color = "#6246ea";
+  console.log(str);
+  switch (str) {
+    case "aboutme":
+      document.getElementById("aboutme").style.opacity = "1";
+      document.getElementById("aboutmelink").style.color = "#e45858";
+      break;
+    case "cvitae":
+      document.getElementById("cvitae").style.opacity = "1";
+      document.getElementById("cvitaelink").style.color = "#e45858";
+      break;
+    case "projects":
+      document.getElementById("projects").style.opacity = "1";
+      document.getElementById("projectslink").style.color = "#e45858";
+      break;
+    case "contact":
+      document.getElementById("contact").style.opacity = "1";
+      document.getElementById("contactlink").style.color = "#e45858";
+      break;
+  }
+}
