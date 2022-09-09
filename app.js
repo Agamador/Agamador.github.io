60b561bb8e32918d7b5bcf0bfdb6e0b801e220a2
Alejandro García <ccmns12@gmail.com>
2022-09-09 11:39:41 +0200

    Cambio paleta de color y posición de las secciones

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ function change(str) {
   document.getElementById("cvitae").style.opacity = "0";
   document.getElementById("projects").style.opacity = "0";
   document.getElementById("contact").style.opacity = "0";
-  document.getElementById("aboutmelink").style.color = "#6246ea";
-  document.getElementById("cvitaelink").style.color = "#6246ea";
-  document.getElementById("projectslink").style.color = "#6246ea";
-  document.getElementById("contactlink").style.color = "#6246ea";
+  document.getElementById("aboutmelink").style.color = "#001e1d";
+  document.getElementById("cvitaelink").style.color = "#001e1d";
+  document.getElementById("projectslink").style.color = "#001e1d";
+  document.getElementById("contactlink").style.color = "#001e1d";
   console.log(str);
   switch (str) {
     case "aboutme":
       document.getElementById("aboutme").style.opacity = "1";
-      document.getElementById("aboutmelink").style.color = "#e45858";
+      document.getElementById("aboutmelink").style.color = "#fba72a";
       break;
     case "cvitae":
       document.getElementById("cvitae").style.opacity = "1";
-      document.getElementById("cvitaelink").style.color = "#e45858";
+      document.getElementById("cvitaelink").style.color = "#fba72a";
       break;
     case "projects":
       document.getElementById("projects").style.opacity = "1";
-      document.getElementById("projectslink").style.color = "#e45858";
+      document.getElementById("projectslink").style.color = "#fba72a";
       break;
     case "contact":
       document.getElementById("contact").style.opacity = "1";
-      document.getElementById("contactlink").style.color = "#e45858";
+      document.getElementById("contactlink").style.color = "#fba72a";
       break;
   }
 }
