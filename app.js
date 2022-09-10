fb5eb438c348ed36e2be6a4de6953724709b60f0
Alejandro García <ccmns12@gmail.com>
2022-09-10 23:17:17 +0200

    Cambio paleta de colores usada Corrección para las diferentes resoluciones Añadido contenido formación

--- app.js
+++ app.js
@@ -3,27 +3,26 @@ function change(str) {
   document.getElementById("cvitae").style.opacity = "0";
   document.getElementById("projects").style.opacity = "0";
   document.getElementById("contact").style.opacity = "0";
-  document.getElementById("aboutmelink").style.color = "#001e1d";
-  document.getElementById("cvitaelink").style.color = "#001e1d";
-  document.getElementById("projectslink").style.color = "#001e1d";
-  document.getElementById("contactlink").style.color = "#001e1d";
-  console.log(str);
+  document.getElementById("aboutmelink").className = "";
+  document.getElementById("cvitaelink").className = "";
+  document.getElementById("projectslink").className = "";
+  document.getElementById("contactlink").className = "";
   switch (str) {
     case "aboutme":
       document.getElementById("aboutme").style.opacity = "1";
-      document.getElementById("aboutmelink").style.color = "#fba72a";
+      document.getElementById("aboutmelink").className = "activelink";
       break;
     case "cvitae":
       document.getElementById("cvitae").style.opacity = "1";
-      document.getElementById("cvitaelink").style.color = "#fba72a";
+      document.getElementById("cvitaelink").className = "activelink";
       break;
     case "projects":
       document.getElementById("projects").style.opacity = "1";
-      document.getElementById("projectslink").style.color = "#fba72a";
+      document.getElementById("projectslink").className = "activelink";
       break;
     case "contact":
       document.getElementById("contact").style.opacity = "1";
-      document.getElementById("contactlink").style.color = "#fba72a";
+      document.getElementById("contactlink").className = "activelink";
       break;
   }
 }
