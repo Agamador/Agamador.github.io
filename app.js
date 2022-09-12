5a4445f787b71c8433c70f787122a0de4b0b214f
Alejandro García <ccmns12@gmail.com>
2022-09-12 21:35:19 +0200

    Contacto y finalización formación

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ function change(str) {
   document.getElementById("cvitae").style.opacity = "0";
   document.getElementById("projects").style.opacity = "0";
   document.getElementById("contact").style.opacity = "0";
+  document.getElementById("aboutme").style.zIndex = "1";
+  document.getElementById("cvitae").style.zIndex = "1";
+  document.getElementById("projects").style.zIndex = "1";
+  document.getElementById("contact").style.zIndex = "1";
   document.getElementById("aboutmelink").className = "";
   document.getElementById("cvitaelink").className = "";
   document.getElementById("projectslink").className = "";
@@ -10,18 +14,22 @@ function change(str) {
   switch (str) {
     case "aboutme":
       document.getElementById("aboutme").style.opacity = "1";
+      document.getElementById("aboutme").style.zIndex = "99";
       document.getElementById("aboutmelink").className = "activelink";
       break;
     case "cvitae":
       document.getElementById("cvitae").style.opacity = "1";
+      document.getElementById("cvitae").style.zIndex = "99";
       document.getElementById("cvitaelink").className = "activelink";
       break;
     case "projects":
       document.getElementById("projects").style.opacity = "1";
+      document.getElementById("projects").style.zIndex = "99";
       document.getElementById("projectslink").className = "activelink";
       break;
     case "contact":
       document.getElementById("contact").style.opacity = "1";
+      document.getElementById("contact").style.zIndex = "99";
       document.getElementById("contactlink").className = "activelink";
       break;
   }
